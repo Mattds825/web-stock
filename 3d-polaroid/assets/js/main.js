831b3db5a60fc4c939aecdc2ca321a4b4e6d4095
Matthew Da Silva <matthewgdasilva25@gmail.com>
2024-10-10 13:59:49 +0100

    add 3d polaroid folder

---
+++ 3d-polaroid/assets/js/main.js
@@ -0,0 +1,38 @@
+const section = document.querySelector(".book-showcase");
+const book = document.querySelector(".book");
+const body = document.querySelector("body");
+
+// pervious rotation value
+let prev = 0;
+
+// calculation (stores calculation formula for tracking the mouse and book rotation)
+let calc = 0;
+
+// drag sensitivity (the higher the number the less sensitive the drag)
+const sensitivity = 2;
+
+// Get the x position of the mouse when the mouse is clicked down
+section.addEventListener("mousedown", (e) => {
+  const x = e.clientX;
+
+  // rotate the book on mousemove
+  section.addEventListener("mousemove", rotate);
+
+  function rotate(e) {
+    calc = (e.clientX - x) / sensitivity;
+
+    // rotate the book from the previous rotation value
+    book.style.transform = `rotateY(${prev + calc}deg)`;
+
+    // change the cursor to grabbing
+    body.style.cursor = "grabbing";
+  }
+  // save rotation for the next click
+  prev += calc;
+
+  // remove the mousemove event listener when the mouse is released
+  window.addEventListener("mouseup", () => {
+    section.removeEventListener("mousemove", rotate);
+    body.style.cursor = "default";
+  });
+});
